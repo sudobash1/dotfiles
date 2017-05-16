60a31d3616263a14c1767a4f546677b0d35569e7
Stephen Robinson <stephen.robinson@eqware.net>
2017-05-16 11:20:14 -0700

    added pmi_prioxy to xstg k alias

--- local/xstg.bash
+++ local/xstg.bash
@@ -1,3 +1,3 @@
 alias r='pkill -9 "gdb|xstg|fsim" && fg ; stty sane'
-alias k='pkill -9 "gdb|xstg|fsim"'
+alias k='pkill -9 "gdb|xstg|fsim|pmi_proxy"'
 alias err_check='grep -iE "error|fail|abend|inval|unali" *'
