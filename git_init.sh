ca4638da89a165b1f96ad3b44dca0b2a95952320
Stephen Robinson <sblazerobinson@gmail.com>
2017-06-27 09:55:58 -0700

    Called git_init.sh from init.sh

--- git_init.sh
+++ git_init.sh
@@ -1,5 +1,4 @@
 git config --global user.name "Stephen Robinson"
-git config user.email "sblazerobinson@gmail.com"
 git config --global core.editor vim
 git config --global diff.tool vimdiff
 git config --global push.default nothing
--- init.sh
+++ init.sh
@@ -60,6 +60,16 @@ echo "using $(get_local_file bash) as local bash config"
 ln -s $(get_local_file bash) .bashrc.local.bash
 echo
 
+# GIT
+if [ -e .gitconfig ]; then
+  echo "$HOME/.gitconfig already exists."
+  echo "Skipping git configuration"
+else
+  echo "Calling git_init.sh"
+  $dir/git_init.sh
+fi
+echo
+
 if [[ -e $dir/vim/bundle/Vundle.vim/.git ]]; then
   echo "Skipping Vundle initialization"
 else
