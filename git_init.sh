f005129f7a9992f02db431207ce47c2626b4c090
Stephen Robinson <sblazerobinson@gmail.com>
2017-06-27 09:52:43 -0700

    Added git_init.sh

---
+++ git_init.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Stephen Robinson"
+git config user.email "sblazerobinson@gmail.com"
+git config --global core.editor vim
+git config --global diff.tool vimdiff
+git config --global push.default nothing
+git config --global color.ui always
