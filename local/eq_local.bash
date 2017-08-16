4f52cafa0eb356c3f3e8b78c619e2b4b73cef19c
Stephen Robinson <nix.thinkpenguin@gmail.com>
2017-08-16 11:06:03 -0700

    alias column to column -n

--- local/eq_local.bash
+++ local/eq_local.bash
@@ -1,3 +1,5 @@
 alias beep="ssh oc-corner 'play /usr/share/sounds/ubuntu/stereo/system-ready.ogg' 1>/dev/null 2>/dev/null"
 alias cluster="ssh srobinson@cluster -t 'ssh bar7'"
 source $DOTFILES_REPO/local/xstg.bash
+
+alias column="column -n"
