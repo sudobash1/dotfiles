2c323bd9a1398409655e433ce807cda83399f6c1
Stephen Robinson <stephen.robinson@eqware.net>
2018-07-17 09:35:35 -0700

    changed git color from always to auto

--- git_init.sh
+++ git_init.sh
@@ -2,4 +2,4 @@ git config --global user.name "Stephen Robinson"
 git config --global core.editor vim
 git config --global diff.tool vimdiff
 git config --global push.default nothing
-git config --global color.ui always
+git config --global color.ui auto
