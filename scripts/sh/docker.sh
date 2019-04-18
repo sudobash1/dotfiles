a2f5779b6223e2835bb76209f148ce41fe74d7bb
Stephen Robinson <stephen.robinson@eqware.net>
2019-04-18 10:02:55 -0700

    Updated docker.sh to allow arguments to shell

--- scripts/sh/docker.sh
+++ scripts/sh/docker.sh
@@ -3,30 +3,32 @@ alias docker='sudo docker'
 alias dtime_refresh='sudo docker run --rm --privileged alpine hwclock -s'
 
 function dshell() {
-  if [ "$1" = "-h" ] || [ $# -gt 3 ]; then
-    echo "Usage: dshell <container_name> [user_name] [shell]"
+  if [ "$1" = "-h" ]; then
+    echo "Usage: dshell <container_name> [user_name] [shell [shell arguments]]"
     return 0
   fi
   if [ $# -eq 0 ]; then
     sudo docker ps -a
     return 1
   fi
-  docker_ps=$(sudo docker ps -a | awk '$NF == "'"$1"'"')
+  local docker="$1"
+  local docker_ps="$(sudo docker ps -a | awk '$NF == "'"$docker"'"')"
   if [ "$docker_ps" ]; then
-    sudo docker start "$1"
+    sudo docker start "$docker"
 
-    user="eqware"
+    local user="eqware"
     if [ $# -ge 2 ]; then
-      user=$2
+      local user="$2"
     fi
 
     # Get the user's default shell
-    shell="$(sudo docker exec "$1" getent passwd "$user" | cut -d: -f7)"
-    if [ $# -eq 3 ]; then
-      shell=$3
+    local shell="$(sudo docker exec "$docker" getent passwd "$user" | cut -d: -f7)"
+    if [ $# -ge 3 ]; then
+      local shell="$3"
     fi
+    [ $# -gt 3 ] && shift 3 || shift $#
 
-    sudo docker exec -it -e "TZ=US/Pacific" -e "TERM=$TERM" -u "$user" "$1" "$shell"
+    sudo docker exec -it -e "TZ=US/Pacific" -e "TERM=$TERM" -u "$user" "$docker" "$shell" "$@"
   else
     echo "No such docker container: $1"
   fi
