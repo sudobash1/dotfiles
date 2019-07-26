dfa9b8561b8bde975aa25b382b6d3c0106c89868
Stephen Robinson <stephen.robinson@eqware.net>
2019-07-26 21:21:09 +0000

    local/act-dl.sh: Added paths

--- local/act-dl.sh
+++ local/act-dl.sh
@@ -1,5 +1,6 @@
 export TOOLCHAIN_PATH_ARMV7=$HOME/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf
 export LANG=en_US.UTF-8
+
 function doinit() {
   if [ -n "$ZSH_VERSION" ]; then
     local SBR_SETOPT_SHWORDSPLIT=$(setopt | grep '^shwordsplit$')
@@ -14,3 +15,14 @@ function doinit() {
     fi
   fi
 }
+
+export build="$HOME/tisdk/build"
+export tmp="$build/arago-tmp-external-linaro-toolchain"
+export images="$tmp/deploy/images/am335x-evm"
+export ipk="$tmp/deploy/ipk/armv7ahf-neon"
+export ipkm="$tmp/deploy/ipk/am335x-evm"
+export ipka="$tmp/deploy/ipk/all"
+export builds="$tmp/work/armv7ahf-neon-linux-gnueabi"
+export buildsm="$tmp/work/am335x_evm-linux-gnueabi"
+export buildsa="$tmp/work/all-linux"
+
